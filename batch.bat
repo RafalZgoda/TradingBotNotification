a45405a52a563359028301e3d9119724e0988788
ZgodaRafal <rafal-@hotmail.fr>
2018-03-04 13:10:32 +0100

    first commit

---
+++ batch.bat
@@ -0,0 +1,8 @@
+set toto = $(curl https://bitaps.com/api/hashrate 2>&1)
+echo toto
+
+
+REM curl -s --form-string "token=a8od1dgi1mvxnfbsn7xskxnd4tb5kw" --form-string "user=ucpvxd1x9ogjkvvi1zpqibt5ir71bh" --form-string "message=hello world" https://api.pushover.net/1/messages.json
+
+
+pause
