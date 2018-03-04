909ff6cded07ad2ccb409d1c4365b85e0f86936a
ZgodaRafal <rafal-@hotmail.fr>
2018-03-04 14:38:31 +0100

    add time + log + send notification + cleanup code

--- Old_Testing/batch.bat
+++ Old_Testing/batch.bat
@@ -4,5 +4,5 @@ echo "curl https://bitaps.com/api/hashrate"
 
 REM curl -s --form-string "token=a8od1dgi1mvxnfbsn7xskxnd4tb5kw" --form-string "user=ucpvxd1x9ogjkvvi1zpqibt5ir71bh" --form-string "message=hello world" https://api.pushover.net/1/messages.json
 
-
+node server.js
 pause
